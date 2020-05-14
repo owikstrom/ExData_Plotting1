41ea7a91b3e39b77711c8eaea388386cf4424ad7
Olov Wikstrom <olov.wikstrom@gmail.com>
2020-05-14 16:31:17 +0200

    Implemented Plot 1

--- plot1.R
+++ plot1.R
@@ -1,11 +1,14 @@
+# Data selection
+
 df <- read.csv('household_power_consumption.txt', header =TRUE, sep =";")
-df$Date <- as.Date(df$Date, "%d / %m / %y")
+df <-subset(df, Date == "1/2/2007" | Date == "2/2/2007")
+df$Global_active_power <- as.numeric(df$Global_active_power)
 
-df$Dt <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
-
-df2 <-subset(df, dt >= "2007-2-1" & dt < "2007-2-2")
-rm(df)
-
-hist(df2$Global_active_power, main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', ylab = 'Frequency', col = 'red')
+# Graphing
 
 png('plot1.png', 480, 480, 'px')
+hist(df$Global_active_power, main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', ylab = 'Frequency', col = 'red')
+
+# Cleanup
+rm(df)
+dev.off()
